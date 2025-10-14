7a418adaeb07f10eba137bdbf1f9a57af89aca57
Babu Devandla <babu.devandla1988@gmail.com>
2025-10-14 13:34:50 +0800

    Delivery creation changes
    Delivery creation changes

--- src/main/java/com/dropmate/auth/controller/ItemDeliveryController.java
+++ src/main/java/com/dropmate/auth/controller/ItemDeliveryController.java
@@ -1,5 +1,10 @@
 package com.dropmate.auth.controller;
 
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.dropmate.dto.DeliveryRequest;
-import com.dropmate.dto.RideRequest;
+import com.dropmate.entity.DriverProfile;
+import com.dropmate.entity.Trip;
+import com.dropmate.entity.User;
+import com.dropmate.enums.KycStatus;
+import com.dropmate.enums.TripStatus;
+import com.dropmate.enums.TripType;
+import com.dropmate.enums.VehicleType;
 import com.dropmate.service.DeliveryService;
+import com.dropmate.service.DriverProfileService;
 import com.dropmate.service.TripService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +40,7 @@ public class ItemDeliveryController {
 
 	private final TripService tripService;
 	private final DeliveryService deliveryService;
+	private final DriverProfileService driverProfileService;
 	
 	@ModelAttribute("deliveryRequest")
 	public DeliveryRequest deliveryRequest() {
@@ -57,10 +72,69 @@ public class ItemDeliveryController {
 		return "user/delivery/delivery-date";
 	}
 	
+	@GetMapping("/vehicle")
+	public String vehicle(@ModelAttribute("deliveryRequest") DeliveryRequest request, Model model) {
+		return "user/delivery/delivery-vehicle";
+	}
+	
 	// Optional: Confirm page for Delivery
-	@GetMapping("/delivery/{deliveryId}/confirm")
-	public String confirmDelivery(@PathVariable Long deliveryId, Model model) {
-		model.addAttribute("delivery", deliveryService.getDeliveryById(deliveryId));
-		return "user/publish/delivery-confirm"; // confirmation page
+	@PostMapping("/confirm")
+	public String confirmDelivery(@ModelAttribute("deliveryRequest") DeliveryRequest request, Model model,Principal principal) {
+		
+		try {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    	LocalDate startDate = LocalDate.parse(request.getStartTime(), formatter);
+    	
+    	String username = principal.getName();
+    	User user = driverProfileService.findByUsername(username).orElse(null);
+    	
+    	DriverProfile driverProfile = DriverProfile.builder()
+    			.userId(user.getUserId())
+    			.kycStatus(KycStatus.PENDING)
+    			.vehicleType(VehicleType.valueOf(request.getVehicleType().toUpperCase()))
+    			.build();
+    	
+    	driverProfileService.saveDriverProfile(driverProfile);
+    	
+    	TripStatus rideStatus= null;
+    	if(request.getReturnRideOption()!=null) {
+    		if(request.getReturnRideOption().equalsIgnoreCase("yes"))
+    			rideStatus = TripStatus.COMPLETED;
+    		else if(request.getReturnRideOption().equalsIgnoreCase("later"))
+    			rideStatus = TripStatus.SCHEDULED;
+    	}
+    	ObjectMapper mapper = new ObjectMapper();
+    	 // Save to DB here
+    	Trip trip = Trip.builder()
+			.originName(request.getOriginName())
+			.destinationName(request.getDestinationName())
+			.tripType(TripType.DELIVERY)
+			.status(rideStatus)
+			.startDate(startDate)
+			//.pricePerSeat(new BigDecimal(request.getPrice()))
+			.vehicleType(VehicleType.valueOf(request.getVehicleType().toUpperCase()))
+			.bookingType("INSTANT")
+			.duration(request.getDuration())
+			.distance(request.getDistance())
+			.source(mapper.writeValueAsString(request.getSourceJson()))
+			.destination(mapper.writeValueAsString(request.getDestinationJson()))
+			.build();
+			
+    	Trip tripDB = tripService.createTrip(user.getUserId(), trip, new ArrayList());
+    	
+    	return "redirect:/user/delivery/delivery-sucess/"+ tripDB.getId(); // confirmation page
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+		return null; // confirmation page
+	}
+	
+	@GetMapping("/delivery-sucess/{tripId}")
+	public String success(@PathVariable String tripId, Model model) {
+		
+		 model.addAttribute("tripId", tripId);
+		return "user/delivery/delivery-sucess";
 	}
 }
